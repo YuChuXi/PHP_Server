53f4f934b42c0b0b38d29ccdef1ee94639aa7613
YuChuXi <81864000+yuxibenxi@users.noreply.github.com>
2023-03-05 13:40:31 +0800

    Add files via upload

--- src/main/java/cn/yuchuxi/php/Bot.java
+++ src/main/java/cn/yuchuxi/php/Bot.java
@@ -1,6 +1,9 @@
 package cn.yuchuxi.php;
 
+import cn.nukkit.plugin.Plugin;
 import cn.nukkit.plugin.PluginLogger;
+import cn.nukkit.scheduler.PluginTask;
+import cn.nukkit.scheduler.ServerScheduler;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -11,18 +14,28 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 
-public abstract class Bot {
-    public WebSocketClient wsc;
-    PluginLogger logger;
-    Gson gson;
-    URI wsUri;
+public abstract class Bot<T extends Plugin> {
+    private final Gson gson; // json解析器
+    private final T plugin; // 上级插件
+    private final PluginLogger logger; // logger
+    private final URI botWSURI; // ws uri
+    private final WebSocketClient botWSClient; // ws连接
+    private final ReConnectTask<T> botReConnectTask; // 重连任务
+    private final ServerScheduler scheduler; // 任务调度器
 
-    public Bot(String sws, PluginLogger logger) throws URISyntaxException { // 初始化
-        this.logger = PHP_Server.getInstance().getLogger();
+    private int reconnectTime; // 重连间隔 tick
+    //private int reconnectTimes; // 重联次数 x
+
+    public Bot(String sws, int reconnectTime, T plugin) throws URISyntaxException { // 初始化
+        this.plugin = plugin;
+        this.logger = plugin.getLogger();
         this.gson = new Gson();
-        this.wsUri = new URI(sws);
+        this.botWSURI = new URI(sws);
+        this.botReConnectTask = new ReConnectTask<>(plugin, this);
+        this.scheduler = plugin.getServer().getScheduler();
+        this.reconnectTime = reconnectTime;
 
-        this.wsc = new WebSocketClient(this.wsUri) {
+        this.botWSClient = new WebSocketClient(this.botWSURI) {
             @Override
             public void onOpen(ServerHandshake serverHandshake) {
                 logger.info("The WebSocket server is successfully connected");
@@ -46,7 +59,7 @@ public abstract class Bot {
                                     tree.getAsJsonObject("sender").get("user_id").getAsLong(), tree);
                         }
                     }
-                } catch (NullPointerException pointe) {
+                } catch (NullPointerException point) {
 
                 }
 
@@ -55,37 +68,77 @@ public abstract class Bot {
             @Override
             public void onClose(int i, String s, boolean b) {
                 logger.warning("The WebSocket server is disconnected");
+                logger.warning("The WebSocket will reconnect 在" + reconnectTime + "Tick ");
+                //if (botRCTHandler!=null){
+                //    if(botRCTHandler.isCancelled()){
+                //        botRCTHandler.;
+                //    }
+                //}else {
+                //botRCTHandler =
+                scheduler.scheduleDelayedTask(botReConnectTask, reconnectTime);
+                //}
             }
 
             @Override
             public void onError(Exception e) {
                 logger.error("WebSocket connection throws: " + e.getMessage());
                 e.printStackTrace();
+
+
             }
         };
     }
 
-    public boolean connectServer() {
-        this.wsc.connect();
+    public T getPlugin() {
+        return plugin;
+    }
+
+    public int getReconnectTime() {
+        return reconnectTime;
+    }
+
+    public void setReconnectTime(int reconnectTime) {
+        this.reconnectTime = reconnectTime;
+    }
+
+    public WebSocketClient getBotWSClient() {
+        return this.botWSClient;
+    }
+
+    public URI getBotWSURI() {
+        return botWSURI;
+    }
+
+    public boolean connect() { // 连接
+        if (!botWSClient.isOpen()) {
+            this.botWSClient.connect();
+        }
+        return true;
+    }
+
+    public boolean reconnect() { // 连接
+        if (!botWSClient.isOpen()) {
+            this.botWSClient.reconnect();
+        }
         return true;
     }
 
     public boolean sendMessage(String message, long user, String echo) { // 发消息
-        if (wsc.isOpen()) {
+        if (botWSClient.isOpen()) {
             toGoCQ msg = new toGoCQ();
             msg.action = "send_private_msg";
             msg.echo = echo;
             msg.params.auto_escape = false;
             msg.params.user_id = user;
             msg.params.message = message;
-            wsc.send(gson.toJson(msg));
+            botWSClient.send(gson.toJson(msg));
             return true;
         }
         return false;
     }
 
     public boolean sendGroupMessage(String message, long group, String echo) { // 发群消息
-        if (wsc.isOpen()) {
+        if (botWSClient.isOpen()) {
             toGoCQ msg = new toGoCQ();
             msg.action = "send_group_msg";
             msg.echo = echo;
@@ -95,7 +148,7 @@ public abstract class Bot {
             msg.params.message = message;
 
             //logger.info(gson.toJson(msg));
-            wsc.send(gson.toJson(msg));
+            botWSClient.send(gson.toJson(msg));
             return true;
         }
         return false;
@@ -117,5 +170,23 @@ public abstract class Bot {
             public String message;
             public boolean auto_escape;
         }
+    }
+}
+
+class ReConnectTask<T extends Plugin> extends PluginTask {
+    private final Bot<T> bot;
+    private final T plugin;
+    private final PluginLogger logger;
+
+    public ReConnectTask(T plugin, Bot<T> bot) {
+        super(plugin);
+        this.plugin = plugin;
+        this.logger = plugin.getLogger();
+        this.bot = bot;
+    }
+
+    @Override
+    public void onRun(int i) {
+        bot.reconnect();
     }
 }
--- src/main/java/cn/yuchuxi/php/PHP_Server.java
+++ src/main/java/cn/yuchuxi/php/PHP_Server.java
@@ -1,19 +1,24 @@
 package cn.yuchuxi.php;
 
+import cn.nukkit.Server;
 import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerJoinEvent;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.plugin.PluginLogger;
+import cn.nukkit.utils.Config;
 import com.google.gson.JsonObject;
 
 import java.net.URISyntaxException;
 
 
-public class PHP_Server extends PluginBase implements Listener {
+public class PHP_Server extends PluginBase {
     private static PHP_Server instance;
     private PluginLogger logger;
     private Bot bot;
+    private Config config;
+    private Server server;
 
     static public PHP_Server getInstance() { // 用于获取该插件状态
         return PHP_Server.instance;
@@ -23,17 +28,29 @@ public class PHP_Server extends PluginBase implements Listener {
         return this.bot;
     }
 
+    public Config getConfig() {
+        return this.config;
+    }
+
     @Override
     public void onLoad() {
         this.logger = this.getLogger(); // 保存logger及self
+        this.server = this.getServer();
         PHP_Server.instance = this;
-        logger.info("PHP_Server Loaded.");
+
+        this.saveDefaultConfig();
+        this.config = new Config(this.getDataFolder() + "/config.yml", Config.YAML); // 处理配置文件
+        config.set("server.startimes", config.getInt("server.startimes") + 1);
+        config.save();
 
         try {
-            this.bot = new Bot("ws://127.0.0.1:47862", this.logger) { // 初始化bot
+            this.bot = new Bot<PHP_Server>(config.getString("bot.ws"), config.getInt("bot.reconnectimes"), this) { // 初始化bot
                 @Override
                 public void groupMessageL(String message, long group, long user, JsonObject msg) { // 收到群消息
-
+                    if (group == config.getLong("bot.group.repost")) {
+                        server.broadcastMessage("[§6聊天转发§f]" + msg.getAsJsonObject("sender").
+                                get("nickname").getAsString() + "：" + message);
+                    }
                 }
 
                 @Override
@@ -41,17 +58,18 @@ public class PHP_Server extends PluginBase implements Listener {
 
                 }
             };
-            bot.connectServer(); // 连接服务器
+            bot.connect(); // 连接服务器
 
         } catch (URISyntaxException e) { // ws服务器地址无效？
             logger.error("The URI cannot be generated, please check the Bot configuration");
         }
+        logger.info("PHP_Server Loaded.");
     }
 
     @Override
     public void onEnable() {
-        bot.sendGroupMessage("测试服已开启", 879578995L, "onEnable");
-
+        bot.sendGroupMessage("测试服已开启", config.getLong("bot.group.main"), "onEnable");
+        this.getServer().getPluginManager().registerEvents(new EventsListener(), this);
 
         //RegisteredListener chatListener = new RegisteredListener(listener, executor, priority, this, ignoreCancelled, timing);
         //PlayerChatEvent.getHandlers().register(chatListener);
@@ -62,21 +80,29 @@ public class PHP_Server extends PluginBase implements Listener {
         //插件关闭
     }
 
-    //@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true) // 监听测试
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent e) {
-        logger.info("fvjhfjhfvjhvfhjdsvfhdsvfhjdvhsvfjhvfdjhvdhsvdvfhdvhsvfhdv");
-        e.getPlayer().sendMessage("你好 " + e.getPlayer());
-    }
+
 }
 
-/*
-class ChatListener implements Listener {
-    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = false)
-    public void onChat() {
 
+class EventsListener implements Listener {
+    PHP_Server plugin;
+    PluginLogger logger;
+    Config config;
+    Bot<PHP_Server> bot;
+
+    EventsListener() {
+        this.plugin = PHP_Server.getInstance();
+        this.logger = plugin.getLogger();
+        this.bot = plugin.getBot();
+        this.config = plugin.getConfig();
     }
+
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = false) // 监听测试
+    public void onJoin(PlayerJoinEvent e) {
+        bot.sendGroupMessage("[" + config.getString("server.name") + "] " + e.getPlayer().getName() + " 加入了游戏",
+                config.getLong("bot.group.repost"), "onPlayerJoin");
+    }
+
 }
 
- */
 
